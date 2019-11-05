fd7aa15a350bc13d6a26846e3bc6ad3453ea7a09
ChristiSpehar1 <54651742+ChristiSpehar1@users.noreply.github.com>
2019-11-04 22:01:55 -0500

    Add files via upload

---
+++ Lab2/Lab2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1 = 7;
+            int number2 = 6;
+            Console.WriteLine(number1 * number2);
+            Console.WriteLine("{0} \n{1}", "Hello World!", "From Christi Spehar");
+            if (number1 > number2)
+            {
+                Console.WriteLine( "{0} \n{1}", "Hello World!", "From Christi Spehar");
+            }
+            //a. It is in the main method.
+            //b. An integer is a whole number without a decimal. A double variable is a variable that can hold large numbers including numbers with decimal places. A floating-point variable is a decimal number that can hold real numbers (often times smaller than a double.)
+            //c. An example of a method would be the instance method. an example of this would be 
+                // if (1 > 0)
+                // {
+                // Console.Writeline("1>0")
+                //}
+            //d. get accessor and a set accessor
+            //e. A class is a keyword. It is a general subject. You can create as many instances as you want as long as there is no limitations set in the code. An object is an instance of a class. It is more specific.
+        }
+    }
+}
