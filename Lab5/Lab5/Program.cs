a4a34265ba3f9b6c57db41a5fae98663bebda0c9
ChristiSpehar1 <54651742+ChristiSpehar1@users.noreply.github.com>
2019-11-25 10:17:38 -0500

    Add files via upload

---
+++ Lab5/Lab5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Lab5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+            // public double getArea(double height, double width
+            // {
+            // return height * width;
+            // }
+
+            // A. The scope would be anything within the curly braces. Typically the variable in this case "return height * width" would be the scope. Another possible scope could be "return height - width"
+            // B. In this specific example this would be Non-Static. If it were static, it would specify after "public." Static would relate to the type itself rather than the instance.
+            // C. The return type would be the value the method returns. In this case it would be the answer to height * width. So the getArea. It could change if the formula was height - width.
+            // D. The method name would be getArea. It is the unique identifier. It could differ in this if we changed it to getParameter.
+            // E. The parameters would be the given values in the method. In this case they would be double height and double width. The parameters can change if you made it double height and double height.
+            // F. The method body would set parameters and the name. in this case it would be "public double getArea(double height, double width")
+
+            // a user-defined method is 100% covered by the user. The user creates the class, the instance of the class, the values, etc. When using a user-defined class, you have to make sure you create everything yourself. With the method given with the frame work. It is much more limitted on what you can do.
+
+            // a static method belongs to the class and can be called by the class and an object of the class. while the non static methods belong to an object of the class and can be called from only the object. 
+        }
+    }
+}
